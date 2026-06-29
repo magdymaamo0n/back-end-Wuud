0aecd6266f618a7036b43e671be50bb00b995af6
MagdyElbaz <magdymaamoon17@gmail.com>
2026-06-29 19:00:04 +0300

    update

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,9 +3,6 @@
 namespace Database\Seeders;
 
 use App\Models\User;
-use App\Models\Category;
-use App\Models\Product;
-use App\Models\ProductImage;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -21,19 +18,5 @@ class DatabaseSeeder extends Seeder
                 'role' => 1995
             ]
         );
-
-        // 2. إنشاء 10 أقسام أولاً
-        $categories = Category::factory(10)->create();
-
-        // 3. إنشاء منتجات مربوطة بالأقسام اللي لسه معمولة
-        Product::factory(50)->make()->each(function ($product) use ($categories) {
-            $product->category_id = $categories->random()->id;
-            $product->save();
-
-            // 4. إنشاء صور لكل منتج بالمرة
-            ProductImage::factory(3)->create([
-                'product_id' => $product->id
-            ]);
-        });
     }
 }
